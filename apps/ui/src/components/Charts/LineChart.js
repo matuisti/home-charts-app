36fcb08c78e61769481fa7cdc733ff6a0643e163
Matias Suihkonen <matias.suihkonen@vincit.fi>
2021-03-09 14:41:34 +0200

    fixed date format

--- apps/ui/src/components/Charts/LineChart.js
+++ apps/ui/src/components/Charts/LineChart.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
+import am4lang_fi_FI from "@amcharts/amcharts4/lang/fi_FI";
+
 
 const LineChart = ({ elementId, data }) => {
   const chartRef = useRef(null);
@@ -9,11 +11,20 @@ const LineChart = ({ elementId, data }) => {
       chartRef.current = am4core.create(elementId, am4charts.XYChart);
       chartRef.current.data = data;
       chartRef.current.data = chartRef.current.data.map(element => {
-        element.created_at = new Date(element.created_at * 1000);
+        element.created_at = new Date(parseInt(element.created_at))
         return element;
       });
 
+      chartRef.current.dateFormatter.dateFormat = "d/M/yy";
+      chartRef.current.language.locale = am4lang_fi_FI;
+      chartRef.current.logo.disabled = true;
+
       var dateAxis = chartRef.current.xAxes.push(new am4charts.DateAxis());
+      dateAxis.dateFormats.setKey("day", "d/M/yy");
+      dateAxis.periodChangeDateFormats.setKey("day", "d/M/yy");
+      dateAxis.groupData = true;
+
+
       var valueAxis = chartRef.current.yAxes.push(new am4charts.ValueAxis());
       valueAxis.tooltip.disabled = true;
 
@@ -21,13 +32,15 @@ const LineChart = ({ elementId, data }) => {
       
       var series = chartRef.current.series.push(new am4charts.LineSeries());
       series.dataFields.dateX = "created_at";
-      series.dataFields.openValueY = "temperature";
       series.dataFields.valueY = "humidity";
+      series.dataFields.openValueY = "temperature";
       series.tooltipText = "temperature: {openValueY.value} humidity: {valueY.value}";
       series.sequencedInterpolation = true;
       series.fillOpacity = 0.3;
       series.defaultState.transitionDuration = 1000;
       series.tensionX = 0.8;
+      series.dateFormatter.dateFormat = "d/M/yy";
+      series.name = "Lämpötila °C"
       
       var series2 = chartRef.current.series.push(new am4charts.LineSeries());
       series2.dataFields.dateX = "created_at";
@@ -36,6 +49,8 @@ const LineChart = ({ elementId, data }) => {
       series2.defaultState.transitionDuration = 1500;
       series2.stroke = chartRef.current.colors.getIndex(6);
       series2.tensionX = 0.8;
+      series2.dateFormatter.dateFormat = "d/M/yy";
+      series2.name = "Kosteus %"
       
       chartRef.current.cursor = new am4charts.XYCursor();
       chartRef.current.cursor.xAxis = dateAxis;
