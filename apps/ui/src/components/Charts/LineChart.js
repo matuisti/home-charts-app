1f2e03a96ce15f22b4618c80b8caec5c47e464c7
Matias Suihkonen <matias.suihkonen@mehilainen.fi>
2021-12-18 12:32:17 +0200

    add current temp and humidity data

--- apps/ui/src/components/Charts/LineChart.js
+++ apps/ui/src/components/Charts/LineChart.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
-import am4lang_fi_FI from "@amcharts/amcharts4/lang/fi_FI";
+import am4lang_fi_FI from '@amcharts/amcharts4/lang/fi_FI';
+import { styled } from '@mui/material/styles';
 
+const Chart = styled('div')({
+  width: '100%', 
+  height: 600,
+});
 
 const LineChart = ({ elementId, data }) => {
   const chartRef = useRef(null);
@@ -15,55 +20,84 @@ const LineChart = ({ elementId, data }) => {
         return element;
       });
 
-      chartRef.current.dateFormatter.dateFormat = "d/M/yy";
+      chartRef.current.dateFormatter.dateFormat = 'd/M/yy';
       chartRef.current.language.locale = am4lang_fi_FI;
       chartRef.current.logo.disabled = true;
 
-      var dateAxis = chartRef.current.xAxes.push(new am4charts.DateAxis());
-      dateAxis.dateFormats.setKey("day", "d/M/yy");
-      dateAxis.periodChangeDateFormats.setKey("day", "d/M/yy");
+      let dateAxis = chartRef.current.xAxes.push(new am4charts.DateAxis());
+      dateAxis.dateFormats.setKey('day', 'd/M/yy');
+      dateAxis.periodChangeDateFormats.setKey('day', 'd/M/yy');
       dateAxis.groupData = true;
 
-
-      var valueAxis = chartRef.current.yAxes.push(new am4charts.ValueAxis());
+      let valueAxis = chartRef.current.yAxes.push(new am4charts.ValueAxis());
       valueAxis.tooltip.disabled = true;
-
-      //valueAxis.min = 0;
       
-      var series = chartRef.current.series.push(new am4charts.LineSeries());
-      series.dataFields.dateX = "created_at";
-      series.dataFields.valueY = "humidity";
-      series.dataFields.openValueY = "temperature";
-      series.tooltipText = "temperature: {openValueY.value} humidity: {valueY.value}";
+      let series = chartRef.current.series.push(new am4charts.LineSeries());
+
+      // const bullet = series.bullets.push(new am4charts.CircleBullet());
+      // bullet.circle.stroke = am4core.color("#fff");
+      // bullet.circle.strokeWidth = 4;
+
+      // let circle = bullet.createChild(am4core.Circle);
+      // circle.width = 8;
+      // circle.height = 8;
+      // let bullethover = bullet.states.create("hover");
+      // bullethover.properties.scale = 1.3;
+
+      series.dataFields.dateX = 'created_at';
+      series.dataFields.valueY = 'humidity';
+      series.dataFields.openValueY = 'temperature';
+      series.tooltipText = 'lämpötila: {openValueY.value} kosteus: {valueY.value}';
       series.sequencedInterpolation = true;
-      series.fillOpacity = 0.3;
+      series.fillOpacity = 0.2;
       series.defaultState.transitionDuration = 1000;
       series.tensionX = 0.8;
-      series.dateFormatter.dateFormat = "d/M/yy";
-      series.name = "Lämpötila °C"
+      series.dateFormatter.dateFormat = 'd/M/yy';
+      series.name = 'Kosteus %';
+
+      series.events.once('datavalidated', () => {
+        const lastDate = new Date(data[data.length - 1].created_at);
+        const firstDate = new Date(lastDate.getTime() - 3600000);
+        dateAxis.zoomToDates(firstDate, lastDate);
+      })
       
-      var series2 = chartRef.current.series.push(new am4charts.LineSeries());
-      series2.dataFields.dateX = "created_at";
-      series2.dataFields.valueY = "temperature";
+      let series2 = chartRef.current.series.push(new am4charts.LineSeries());
+      series2.stroke = am4core.color("red");
+      
+      // let bullet2 = series2.bullets.push(new am4charts.CircleBullet());
+      // bullet2.circle.stroke = am4core.color("#fff");
+      // bullet2.circle.strokeWidth = 4;
+
+      // let circle2 = bullet2.createChild(am4core.Circle);
+      // circle2.fill = am4core.color("red");
+      // circle2.width = 8;
+      // circle2.height = 8;
+      // let bullethover2 = bullet2.states.create("hover");
+      // bullethover2.properties.scale = 1.3;
+      
+      series2.dataFields.dateX = 'created_at';
+      series2.dataFields.valueY = 'temperature';
       series2.sequencedInterpolation = true;
       series2.defaultState.transitionDuration = 1500;
-      series2.stroke = chartRef.current.colors.getIndex(6);
       series2.tensionX = 0.8;
-      series2.dateFormatter.dateFormat = "d/M/yy";
-      series2.name = "Kosteus %"
+      series2.dateFormatter.dateFormat = 'd/M/yy';
+      series2.name = 'Lämpötila °C';
       
       chartRef.current.cursor = new am4charts.XYCursor();
       chartRef.current.cursor.xAxis = dateAxis;
-      chartRef.current.scrollbarX = new am4core.Scrollbar();
+      let scrollbar = new am4charts.XYChartScrollbar();
+      scrollbar.series.push(series);
+      chartRef.current.scrollbarX = scrollbar;
 
       chartRef.current.legend = new am4charts.Legend();
-      chartRef.current.legend.position = "bottom";
+      chartRef.current.legend.position = 'bottom';
     }
 
     return () => {
       chartRef.current && chartRef.current.dispose();
     };
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); 
 
   useEffect(() => {
     if (chartRef.current) {
@@ -78,7 +112,7 @@ const LineChart = ({ elementId, data }) => {
   }, []);
 
   return (
-    <div id={elementId} style={{ width: "100%", height: "700px" }} />
+    <Chart id={elementId} />
   );
 };
 
--- apps/ui/src/components/Overview/Overview.js
+++ apps/ui/src/components/Overview/Overview.js
@@ -1,33 +1,78 @@
 import React, { useEffect, useState } from 'react';
-import { getSensorData } from '../../api/api';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography'
 import LineChart from '../Charts/LineChart';
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
+import InvertColorsIcon from '@mui/icons-material/InvertColors';
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { styled } from '@mui/material/styles';
+import { getSensorData } from '../../api/api';
+
+const CurrentDataGrid = styled(Grid)({
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+const ValueBox = styled(Paper)({
+  padding: 20,
+});
+
+const LineChartContainer = styled(Paper)({
+  marginTop: 20,
+  marginBottom: 40,
+});
 
 const Overview = () => {
   const [state, setState] = useState({
     loading: true,
-    lineChartData: []
+    sensorData: [],
+    current: null
   });
 
   useEffect(() => {
     getSensorData().then((response) => {
       setState({
         ...state,
-        lineChartData: response,
+        sensorData: response,
+        current: response[response.length -1],
         loading: false
       });
     }).catch((err) => {
       console.log(err);
     })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <div>
-      { !state.loading &&
-      <LineChart 
-        elementId={'line-chart-body'}
-        data={state.lineChartData} />}
-    </div>
-  );
+  return !state.loading ? (
+    <Container disableGutters={false} maxWidth="lg">
+      <Box pt={2}>
+        <CurrentDataGrid container spacing={2}>
+          <Grid item xs={6} md={4}>
+            <ValueBox>
+              <DeviceThermostatIcon sx={{ padding: '11px', boxShadow: 6, borderRadius: '50%' }} fontSize="large" />
+              <Typography pt={1} variant="h4">{state.current.temperature}°C</Typography>
+              <Typography>lämpötila nyt</Typography>
+            </ValueBox>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <ValueBox>
+              <InvertColorsIcon sx={{ padding: '11px', boxShadow: 6, borderRadius: '50%' }} fontSize="large" />
+              <Typography pt={1} variant="h4">{state.current.humidity}°C</Typography>
+              <Typography>kosteus nyt</Typography>
+            </ValueBox>
+          </Grid>
+        </CurrentDataGrid>
+        <LineChartContainer xs={12} md={12}>
+          <LineChart 
+            elementId='line-chart-body'
+            data={state.sensorData} 
+          />
+        </LineChartContainer>
+      </Box>
+    </Container>
+  ) : null;
 };
 
 export default Overview;
